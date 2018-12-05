4477c5f2fff835ccf9f8468f034b613d46843067
stephrost <smfrost1023@gmail.com>
2018-12-05 14:24:45 -0500

    pq1a.181205.002 -> android-prepare-vendor

--- muskie/proprietary/BoardConfigVendor.mk
+++ muskie/proprietary/BoardConfigVendor.mk
@@ -1,4 +1,4 @@
-# [2018-11-24] Auto-generated file, do not edit
+# [2018-12-05] Auto-generated file, do not edit
 
 TARGET_BOARD_INFO_FILE := vendor/google_devices/walleye/vendor-board-info.txt
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
